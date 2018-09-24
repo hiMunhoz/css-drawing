f8f7b670ce28b8d4524a561c0bc04f4fc60aa54c
Marcelo Munhoz <me@marcelomunhoz.com>
2018-09-24 14:40:51 -0300

    Favicon

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,7 +2,8 @@ module.exports = {
   title: 'CSS drawing',
   description: 'How to get a beatiful draw with pure HTML and CSS',
   head: [
-    ['meta', { name: 'keywords', content: 'drawing, creativity, css, html, desenho, rascunho' }]
+    ['meta', { name: 'keywords', content: 'drawing, creativity, css, html, desenho, rascunho' }],
+    ['link', { rel: 'icon', href: 'https://res.cloudinary.com/marcelo-munhoz/v1527879459/static/images/favicon.png' }]
   ],
   themeConfig: {
     sidebar: [
