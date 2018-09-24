4f6c4341009c1b3888415e3d21ecd676e4c86023
Marcelo Munhoz <me@marcelomunhoz.com>
2018-09-24 13:36:00 -0300

    Keywords just added

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   title: 'CSS drawing',
   description: 'How to get a beatiful draw with pure HTML and CSS',
+  keywords: ['drawing', 'creativity', 'css', 'html', 'desenho', 'rascunho'],
   themeConfig: {
     sidebar: [
       '/',
